931083a7b8f1da8fe27a387af806d9d1a0dbc540
AzerQ <46249148+AzerQ@users.noreply.github.com>
2023-05-22 11:01:11 +0300

    Diagrams created

---
+++ Lab2.hpp
@@ -0,0 +1,7 @@
+/* This file was automatically generated.  Do not edit! */
+#undef INTERFACE
+int main(int argc,char const *argv[]);
+void Task3();
+void Task2();
+void Task1();
+extern using namespace std;
